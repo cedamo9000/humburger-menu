eb96f1e0dcf789278dec4d143aab92a387d59fe2
Castro Daniel <104678727+cedamo9000@users.noreply.github.com>
2022-10-28 09:03:55 -0700

    changes

--- tutor/login2.js
+++ tutor/login2.js
@@ -1,22 +1,17 @@
-const loginform = document.querySelector('loginform');
-loginform.addEventListener('submit', formsubmit);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
 
-function formsubmit(e) {
-    e.preventDefault();
+const firebaseConfig = {
+    apiKey: "AIzaSyB7Uh0N3WkLkcUN5PQKiXnVKNf3_4JxXa4",
+    authDomain: "flipflop-faac3.firebaseapp.com",
+    databaseURL: "https://flipflop-faac3-default-rtdb.firebaseio.com",
+    projectId: "flipflop-faac3",
+    storageBucket: "flipflop-faac3.appspot.com",
+    messagingSenderId: "82173479146",
+    appId: "1:82173479146:web:064eb6789fb925252a2f93",
+    measurementId: "G-B3DQCK8GW1"
+};
 
-}
-
-function validate() {
-    var x = "please enter email";
-    var y = "please enter password"
-    var error = document.querySelector('.error');
-    error.style.display = "block";
-
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    if (email === "") {
-        console.log("vr");
-    } else {
-        document.getElementById('error').innerHTML = x;
-    }
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
