3cc1a3b1d792e0fe39d924c1bfae731b9a2c952a
baraka_yudah <104678727+cedamo9000@users.noreply.github.com>
2022-11-02 08:33:06 -0700

    code worked but refused to inject to real-database

--- tutor/dist/login2.dev.js
+++ tutor/dist/login2.dev.js
@@ -1,7 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
+"use strict";
 
-const firebaseConfig = {
+var _firebaseApp = require("https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js");
+
+var _firebaseAnalytics = require("https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js");
+
+var firebaseConfig = {
   apiKey: "AIzaSyB7Uh0N3WkLkcUN5PQKiXnVKNf3_4JxXa4",
   authDomain: "flipflop-faac3.firebaseapp.com",
   databaseURL: "https://flipflop-faac3-default-rtdb.firebaseio.com",
@@ -9,18 +12,15 @@ const firebaseConfig = {
   storageBucket: "flipflop-faac3.appspot.com",
   messagingSenderId: "82173479146",
   appId: "1:82173479146:web:064eb6789fb925252a2f93",
-  measurementId: "G-B3DQCK8GW1",
-};
+  measurementId: "G-B3DQCK8GW1"
+}; // Initialize Firebase
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+var app = (0, _firebaseApp.initializeApp)(firebaseConfig);
+var analytics = (0, _firebaseAnalytics.getAnalytics)(app);
 document.getElementById("loginform").addEventListener("submit", submitform);
 
 function submitform(e) {
   e.preventDefault();
-
   var email = getInputVal("email");
   var password = getInputVal("password");
 }
